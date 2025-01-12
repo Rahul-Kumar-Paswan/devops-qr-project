820bc91e097224bc532d6625051ea00ee17554e6
Rahul-Kumar-Paswan <rahulkumarpaswan941+github@gmail.com>
2025-01-12 19:34:05 +0530

    running on ec2 destroying resources

--- Infra/modules/eks/main.tf
+++ Infra/modules/eks/main.tf
@@ -1,7 +1,3 @@
-# resource "aws_internet_gateway" "devops_igw" {
-#   vpc_id = var.vpc_id
-# }
-
 data "aws_vpc" "existing_vpc" {
   id = var.vpc_id
 }
@@ -30,28 +26,3 @@ resource "aws_eks_node_group" "devops_node_group" {
   instance_types = ["t3.medium"]
   depends_on = [aws_eks_cluster.devops_cluster]
 }
-
-
-# resource "eks" "devops_cluster" {
-
-#   cluster_name    = var.cluster_name
-#   cluster_version = var.cluster_version
-
-#   cluster_endpoint_public_access = true
-#   count = var.no_of_subnets
-
-#   vpc_id                   = var.vpc_id
-
-#   subnet_ids = [for i in range(var.no_of_subnets) : aws_subnet.devops_public_subnet[i].id]
-#   control_plane_subnet_ids = [for i in range(var.no_of_subnets) : aws_subnet.devops_public_subnet[i].id]
-
-#   eks_managed_node_groups = {
-#     eks_nodes = {
-#       desired_capacity = 2
-#       max_capacity     = 3
-#       min_capacity     = 1
-
-#       instance_type = "t3.medium"
-#     }
-#   }
-# }
